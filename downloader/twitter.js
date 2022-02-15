7330b5edb8107f01854ae91da94a9f2c7011d4f6
Skh <gansrey97@gmail.com>
2022-02-15 23:19:14 +0700

    Create twitter.js

---
+++ downloader/twitter.js
@@ -0,0 +1,24 @@
+function twitter(url) {
+    return new Promise((resolve, reject) => {
+        let params = new URLSearchParams()
+        params.append('URL', url)
+        fetch('https://twdown.net/download.php', { method: 'POST', body: params })
+            .then(res => res.text())
+            .then(res => {
+                const $ = cheerio.load(res);
+                data = []
+                $('div.container').find('tbody > tr > td').each(function (index, element) {
+                    x = $(this).find('a').attr('href')
+                    if (x !== '#') {
+                        if (typeof x !== 'undefined') {
+                            data.push({ url: x })
+                        }
+                    }
+                })
+                if (data.length == 0) return resolve({ status: false })
+                resolve({ status: true, data })
+            }).catch(reject)
+    })
+}
+
+// module.export = ?
