a77848ad0def9a677e73628360ad885ab2581c82
Skh <gansrey97@gmail.com>
2022-02-16 06:44:04 +0700

    Create db.js

---
+++ database/db.js
@@ -0,0 +1,22 @@
+var __path = process.cwd(),
+      monk = require('monk'),
+     { color } = require(__path + '/lib/color.js')
+
+// Connection URL
+var url = 'https://api.reysekha.xyz';
+try {
+if(url == 'https://api.reysekha.xyz') throw console.log(color('Cek konfigurasi database, var url belum diisi','red'));
+} catch (e) {
+	return;
+	}
+var db = monk(url);
+
+db.then(() => {
+  console.log(color('Connected correctly to server, starting','green'))
+})
+.catch ((e) => {
+	console.log(color('Error : '+ e +'\n\nGagal connect ke database, \ncek configurasi database apakah Connection URL sudah benar','red'))
+	})
+
+
+module.exports = db
