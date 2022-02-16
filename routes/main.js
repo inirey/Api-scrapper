9cbb40126fdd6e96cc8df996faa6ec571733e328
Skh <gansrey97@gmail.com>
2022-02-16 07:22:47 +0700

    Update main.js

--- routes/main.js
+++ routes/main.js
@@ -4,17 +4,10 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/docs', (req, res) => {
-    res.sendFile(__path + '/views/index.html')
+    res.sendFile(__path + '/anjay/index.html')
 })
 router.get('/', (req, res) => {
-    res.sendFile(__path + '/views/index.html')
-})
-router.get('/api/game', (req, res) => {
-    res.sendFile(__path + '/views/index.html')
-})
-
-router.get('/about', (req, res) => {
-    res.sendFile(__path + '/views/index.html')
+    res.sendFile(__path + '/anjay/index.html')
 })
 
 module.exports = router
